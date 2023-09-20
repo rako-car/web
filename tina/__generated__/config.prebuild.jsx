c75c43804d12644b713bf0a5ff24c6e045ba4884
grzegorz.borowik <grzegorzborowik@hotmail.com>
2023-09-20 09:20:27 +0200

    Add tina CMS

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,55 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: process.env.CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "."
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "."
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "services",
+        label: "Serwisy",
+        path: "_services",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+            indexed: true
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Kolejno\u015B\u0107 wy\u015Bwietlania",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
---
+++ tina/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: ".",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: ".",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "services",
+        label: "Serwisy",
+        path: "_services",
+        format: 'md',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+            indexed: true,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Kolejność wyświetlania",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
